d03ec33959f36d5d52e7d4e1977dd0a1c1bc2cef
Damien Clarke <dxinteractive@gmail.com>
2020-08-26 18:27:26 +1000

    feat: add argsToKey function

---
+++ packages/mobx-fog-of-war/src/argsToKey.ts
@@ -0,0 +1,26 @@
+export const argsToKey = (args: unknown): string => {
+
+    // process only collections (objects and arrays)
+    if (args instanceof Object) {
+
+        // make entries
+        const entries: Array<[number|string, unknown]> = [];
+        for (const key in args) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            const value: unknown = args[key];
+            if(value !== undefined && value !== null) {
+                entries.push([key, value]);
+            }
+        }
+
+        // sort by key so differences in key order don't produce different results
+        entries.sort((a, b) => a[0] < b[0] ? -1 : 1);
+
+        // recurse to children
+        args = entries.map(([key, value]) => [key, argsToKey(value)]);
+    }
+
+    // stringify result
+    return JSON.stringify(args);
+};
---
+++ packages/mobx-fog-of-war/test/argsToKey.test.ts
@@ -0,0 +1,22 @@
+import {argsToKey} from '../src/index';
+
+describe('argsToKey', () => {
+    it('should produce different outputs for different params', () => {
+        expect(argsToKey({a:1,b:2})).toEqual(argsToKey({a:1,b:2}));
+        expect(argsToKey({a:1,b:2})).not.toEqual(argsToKey({a:1,b:3}));
+        expect(argsToKey({a:1,b:2})).not.toEqual(argsToKey({a:1,b:"2"}));
+    });
+
+    it('should treat objects with different orders as the same', () => {
+        expect(argsToKey({a:1,b:2,c:3})).toEqual(argsToKey({c:3,b:2,a:1}));
+    });
+
+    it('should process deeply', () => {
+        expect(argsToKey({bb: true, aa: {a:1,b:2,c:3}})).toEqual(argsToKey({aa: {c:3,b:2,a:1}, bb: true}));
+    });
+
+    it('should treat missing keys, keys with null values and keys with undefined values as equivalent', () => {
+        expect(argsToKey({a:null,b:1})).toEqual(argsToKey({b:1}));
+        expect(argsToKey({a:undefined,b:1})).toEqual(argsToKey({b:1}));
+    });
+});
