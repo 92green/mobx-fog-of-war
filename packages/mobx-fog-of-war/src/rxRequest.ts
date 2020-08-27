5c9194425ec2664ce50e2035aab66aabf2a11bdd
Damien Clarke <dxinteractive@gmail.com>
2020-08-27 20:15:21 +1000

    fix: fix types on rxRequest by changing api

--- packages/mobx-fog-of-war/src/rxRequest.ts
+++ packages/mobx-fog-of-war/src/rxRequest.ts
@@ -1,20 +1,17 @@
 import {toJS} from 'mobx';
 import {toStream} from 'mobx-utils';
-import type {Store, NextRequest} from './Store';
+import type {Store, NextRequest, Receive} from './Store';
 import {map} from 'rxjs/operators';
-import {from, pipe} from 'rxjs';
+import {from} from 'rxjs';
 import type {OperatorFunction} from 'rxjs';
 
-// rxjs demans the use of any types
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const rxRequest = <Args,Data,Err>(...operators: Array<OperatorFunction<any, any>>) => (store: Store<Args,Data,Err>): void => {
-    from(toStream((): NextRequest<Args> => toJS(store.nextRequest) as NextRequest<Args>))
-        .pipe(
-            map(item => item.args),
-            // rxjs doesnt like spreading into a pipe
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            pipe(...operators)
-        )
-        .subscribe(received => store.receive(received));
+export const rxRequest = <Args,Data,Err>(operator: OperatorFunction<Args, Receive<Args,Data,Err>>) => {
+    return (store: Store<Args,Data,Err>): void => {
+        from(toStream((): NextRequest<Args> => toJS(store.nextRequest) as NextRequest<Args>))
+            .pipe(
+                map(item => item.args),
+                operator
+            )
+            .subscribe(received => store.receive(received));
+    };
 };
