d0229ea3e4bc2996fa943226503117328e041241
dxinteractive <dxinteractive@users.noreply.github.com>
2020-09-01 07:55:38 +0000

    deploy: a78766a3bd849779330ebb63cc60752a1d4f3597

---
+++ _next/static/G8r4ql4329xdDDvLHNPdo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F8313d723.375cc2be0499eef71952.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
