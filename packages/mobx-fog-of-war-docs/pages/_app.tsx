faa444dff4061a692c62c20be939086cd783ef65
Damien Clarke <dxinteractive@gmail.com>
2020-09-01 17:57:59 +1000

    docs: add extremely basic docs style

--- packages/mobx-fog-of-war-docs/pages/_app.tsx
+++ packages/mobx-fog-of-war-docs/pages/_app.tsx
@@ -1,4 +1,5 @@
 import {AppProps} from 'next/app';
+import '../styles.css';
 
 export default function App({Component, pageProps}: AppProps): React.ReactElement {
     return <Component {...pageProps} />;
