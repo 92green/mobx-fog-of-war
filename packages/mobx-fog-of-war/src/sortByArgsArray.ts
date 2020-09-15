1d4d5367dac9f02d64d8f72f8cb0cdff33e149c4
Damien Clarke <dxinteractive@gmail.com>
2020-09-15 13:45:31 +1000

    break: add getData to rxBatch

--- packages/mobx-fog-of-war/src/sortByArgsArray.ts
+++ packages/mobx-fog-of-war/src/sortByArgsArray.ts
@@ -1,20 +1,22 @@
 import {argsToKey} from './argsToKey';
 import type {Receive} from './Store';
 
-export type GetArgs<A,D> = (data: D) => A;
+export type GetArgs<A,R> = (result: R) => A;
+export type GetData<D,R> = (result: R) => D;
 export type MissingError<A,E> = (args: A) => E;
 
-export const sortByArgsArray = <A,D,E>(
+export const sortByArgsArray = <A,D,E,R>(
     argsArray: A[],
-    dataArray: D[],
-    getArgs: GetArgs<A,D>,
+    resultArray: R[],
+    getArgs: GetArgs<A,R>,
+    getData: GetData<D,R>,
     missingError: MissingError<A,E>
 ): Receive<A,D,E>[] => {
 
-    const dataByKey = new Map<string,D>();
+    const dataByKey = new Map<string,R>();
 
-    dataArray.forEach((data: D) => {
-        dataByKey.set(argsToKey(getArgs(data)), data);
+    resultArray.forEach((result: R) => {
+        dataByKey.set(argsToKey(getArgs(result)), result);
     });
 
     return argsArray.map((args: A): Receive<A,D,E> => {
@@ -23,7 +25,7 @@ export const sortByArgsArray = <A,D,E>(
             return {
                 args,
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                data: dataByKey.get(key)!
+                data: getData(dataByKey.get(key)!)
             };
         }
         return {
