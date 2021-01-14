b1a64319ec7bfdd090c433d82e876889eb8d846d
Damien Clarke <dxinteractive@gmail.com>
2021-01-14 16:30:47 +1100

    fix: add missing observer

--- packages/mobx-fog-of-war/src/Load.tsx
+++ packages/mobx-fog-of-war/src/Load.tsx
@@ -114,14 +114,8 @@ export type LoadProps<D1,E1,D2,E2,D3,E3,D4,E4,D5,E5> = LoadProps5<D1,E1,D2,E2,D3
     |LoadProps1<D1,E1>
     |LoadPropsArray<D1,E1>;
 
-type LoadComponentProps = {
-    renderer: () => ChildrenReturn;
-};
-
 // render children into a component, so children rendered can use their own hooks and have their own lifecycles
-function LoadComponent(props: LoadComponentProps): React.ReactElement {
-    return <>{props.renderer()}</>;
-}
+const LoadComponent = observer(props => <>{props.renderer()}</>);
 
 function LoadInner<D1,E1,D2,E2,D3,E3,D4,E4,D5,E5>(props: LoadProps<D1,E1,D2,E2,D3,E3,D4,E4,D5,E5>): React.ReactElement|null {
     const {
