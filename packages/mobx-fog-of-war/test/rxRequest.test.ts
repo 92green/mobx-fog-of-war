94fcc45df9b2dbe0488a76358fd393397d66b00d
Damien Clarke <dxinteractive@gmail.com>
2021-10-28 14:43:42 +1100

    feat: add rxrequesteach

--- packages/mobx-fog-of-war/test/rxRequest.test.ts
+++ packages/mobx-fog-of-war/test/rxRequest.test.ts
@@ -1,4 +1,4 @@
-import {Store, rxRequest} from '../src/index';
+import {Store, rxRequest, rxRequestEach} from '../src/index';
 import {mergeMap} from 'rxjs/operators';
 import {mocked} from 'ts-jest/utils';
 import {autorun, toJS} from 'mobx';
@@ -79,5 +79,96 @@ describe('rxRequest', () => {
         expect(mocked(changes).mock.calls[1][0].loading).toBe(true);
         expect(mocked(changes).mock.calls[2][0].loading).toBe(false);
         expect(mocked(changes).mock.calls[2][0].error).toBe('ARGH!');
+    });
+});
+
+describe('rxRequestEach', () => {
+    it('should stream changes through observable and receive data', async () => {
+
+        const changes = jest.fn();
+
+        const placeStore = new Store<number,string,string>({
+            request: rxRequestEach(
+                mergeMap(async (args: number) => {
+                    await Promise.resolve();
+                    return `data for ${args}`;
+                })
+            )
+        });
+
+        autorun(() => {
+            changes(toJS(placeStore.read(1)));
+        });
+
+        placeStore.request(1);
+
+        // resolve all promises so that data is returned from mergeMap()
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(mocked(changes)).toHaveBeenCalledTimes(3);
+        expect(mocked(changes).mock.calls[0][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[1][0].loading).toBe(true);
+        expect(mocked(changes).mock.calls[2][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[2][0].data).toBe('data for 1');
+
+        placeStore.request(1);
+
+        // resolve all promises so that data is returned from mergeMap()
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(mocked(changes)).toHaveBeenCalledTimes(5);
+        expect(mocked(changes).mock.calls[3][0].loading).toBe(true);
+        expect(mocked(changes).mock.calls[4][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[4][0].data).toBe('data for 1');
+    });
+
+    it('should stream changes through observable and receive error', async () => {
+
+        const changes = jest.fn();
+        let first = true;
+
+        const placeStore = new Store<number,string,string>({
+            request: rxRequestEach(
+                mergeMap(async (args) => {
+                    if(first) {
+                        first = false;
+                        await Promise.reject('ARGH!');
+                    } else {
+                        await Promise.resolve();
+                    }
+                    return `data for ${args}`;
+                })
+            )
+        });
+
+        autorun(() => {
+            changes(toJS(placeStore.read(1)));
+        });
+
+        placeStore.request(1);
+
+        // resolve all promises so that data is returned from mergeMap()
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(mocked(changes)).toHaveBeenCalledTimes(3);
+        expect(mocked(changes).mock.calls[0][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[1][0].loading).toBe(true);
+        expect(mocked(changes).mock.calls[2][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[2][0].error).toBe('ARGH!');
+
+        placeStore.request(1);
+
+        // resolve all promises so that data is returned from mergeMap()
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(mocked(changes)).toHaveBeenCalledTimes(5);
+        expect(mocked(changes).mock.calls[3][0].loading).toBe(true);
+        expect(mocked(changes).mock.calls[4][0].loading).toBe(false);
+        expect(mocked(changes).mock.calls[4][0].data).toBe('data for 1');
+        expect(mocked(changes).mock.calls[4][0].error).toBe(undefined);
     });
 });
